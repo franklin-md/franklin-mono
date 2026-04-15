6315a1703995ac30af0def527ea37d2005856dfe
A-F-V <faracea2000@gmail.com>
2026-04-15 15:14:17 +0100

    global vitest aliases

--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -1,6 +1,11 @@
 import { configDefaults, defineConfig } from 'vitest/config';
 
+import { franklinVitestAliases } from '../../vitest.aliases';
+
 export default defineConfig({
+	resolve: {
+		alias: franklinVitestAliases(),
+	},
 	test: {
 		exclude: [...configDefaults.exclude, 'dist/**'],
 	},
